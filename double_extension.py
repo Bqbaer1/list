b011c9a8cb802baf2414cc9bc9a78960cffb9f36
Chris <baerbaerchris@gmail.com>
2023-06-06 19:07:03 +0200

    basis

--- double_extension.py
+++ double_extension.py
@@ -1,20 +1,26 @@
 import requests
 
-# das ist die Liste für php extensions unter folgendem link
-# response = requests.get("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/web-extensions.txt")
-extensions = ['.php', '.phtml', '.php4', '.php5', '.php7', '.phps', '.php3', '.phps', '.phpt', '.phar']
+# die Liste aus dem repo enthält extensions für php und asp
+response = requests.get("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/web-extensions.txt")
 
+# splitline() ist Methode des Zeichentyp Strings, teilt inhalt in eine Liste von Zeilen auf
+extensions = response.text.splitlines()
+# Schleife, welche die liste ausgibt
+for i in extensions:
+    print(i)
 
+# Umgang mit Dateien erstellt eine Datei, und öffnet diese, das "w" bedeutet im Modus beschreiben
+# anschließend am ende einer with-Anweisung wird die Datei automatisch geschlossen
 with open("wordlist.txt", "w") as file:
-    # special_chars = ['%20', '%0a', '%00', '%0d0a', '/', '.\\', '.', '…', ':']
+    special_chars = ['%20', '%0a', '%00', '%0d0a', '/', '.\\', '.', '…', ':']
 
-
+    for char in special_chars:
         for ext in extensions:
-         #   file.write(f"shell{char}{ext}.jpg\n")
-          #  file.write(f"shell{ext}{char}.jpg\n")
-           # file.write(f"shell.jpg{char}{ext}\n")
-            #file.write(f"shell.jpg{ext}{char}\n")
-           
-           file.write(f"shell{ext}.jpg\n")
-          
-           file.write(f"shell.jpg{ext}\n")
+            # das f steht für formatieren des strings und ermöglicht, es ermöglicht die 
+            # Auswertung innerhalb des String in {}-Klammern 
+            # file bezieht sich auf das file-Objekt, welches durch die open()-Funktion erstellt wurde
+            # in diesem Fall wordlist.txt
+            file.write(f"shell{char}{ext}.jpg\n")
+            file.write(f"shell{ext}{char}.jpg\n")
+            file.write(f"shell.jpg{char}{ext}\n")
+            file.write(f"shell.jpg{ext}{char}\n")
